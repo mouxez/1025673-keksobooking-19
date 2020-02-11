c553784520b9f1d208787a9c981278ddb760e3a0
Maksim <mouxez@gmail.com>
2020-02-11 11:47:07 +0300

    change the way a random object is generated

--- js/main.js
+++ js/main.js
@@ -2,22 +2,18 @@
 
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
-var advAmount = 8;
+var ADV_AMOUNT = 8;
 var typeOptions = ['palace', 'flat', 'house', 'bungalo'];
 var timeOptions = ['12:00', '13:00', '14:00'];
 var featureOptions = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photoOptions = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var descriptionOptions = ['Светлая и комфортная квартира', 'Близко к метро', 'Чудесный вид на сад', 'Новый ремонт'];
 
-// Вспомогательные функции
 var getRandomFromSegment = function (min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1);
-
-  return Math.round(rand);
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
 
 var chooseRandomElement = function (array) {
-
   return array[Math.floor(Math.random() * array.length)];
 };
 
@@ -30,44 +26,37 @@ var getRandomArray = function (array) {
   return arrItems;
 };
 
-// 1. создаёт свойство аuthor
-var createAuthor = function () {
-  var avatar = 'img/avatars/user' + '0' + Math.floor(Math.random() * 8) + '.png'; // доработка неповтора
-
-  return {avatar: avatar};
-};
-// создаёт свойство оffer
-var createOffer = function () {
-  var title = 'Сдаётся квартира';
-  var address = '600, 350';
-  var price = getRandomFromSegment(30000, 100000) + 'руб.';
-  var type = chooseRandomElement(typeOptions);
-  var rooms = getRandomFromSegment(1, 10);
-  var guests = getRandomFromSegment(1, 10);
-  var checkin = chooseRandomElement(timeOptions);
-  var checkout = chooseRandomElement(timeOptions);
-  var features = getRandomArray(featureOptions);
-  var description = chooseRandomElement(descriptionOptions);
-  var photos = getRandomArray(photoOptions);
-
-  return {title: title, address: address, price: price, type: type, rooms: rooms, guests: guests, checkin: checkin, checkout: checkout, features: features, description: description, photos: photos};
-};
-// создаёт свойство location
-var createLocation = function () {
-  var x = getRandomFromSegment(1, 1200);
-  var y = getRandomFromSegment(130, 630);
-
-  return {x: x, y: y};
-};
 // создаёт один рандомный объект
 var advObject = function () {
-  return {author: createAuthor(), offer: createOffer(), location: createLocation()};
+  return {
+    author:
+  {avatar: 'img/avatars/user' + '0' + (i + 1) + '.png'},
+
+    offer:
+  {title: 'Сдаётся квартира',
+    address: '600, 350',
+    price: getRandomFromSegment(30000, 100000) + 'руб.',
+    type: chooseRandomElement(typeOptions),
+    rooms: getRandomFromSegment(1, 10),
+    guests: getRandomFromSegment(1, 10),
+    checkin: chooseRandomElement(timeOptions),
+    checkout: chooseRandomElement(timeOptions),
+    features: getRandomArray(featureOptions),
+    description: chooseRandomElement(descriptionOptions),
+    photos: getRandomArray(photoOptions)},
+
+    location:
+  {x: getRandomFromSegment(1, 1200),
+    y: getRandomFromSegment(130, 630)}
+  };
 };
+
 // создаёт цикл генерации 8 рандомных объектов
 var randomAd = [];
-for (var i = 0; i <= advAmount; i++) {
+for (var i = 0; i < ADV_AMOUNT; i++) {
   randomAd[i] = advObject();
 }
+
 // 2. убирает класс .map--faded у блока с картой
 document.querySelector('.map').classList.remove('map--faded');
 
@@ -78,7 +67,7 @@ var template = document.querySelector('#pin').content.querySelector('.map__pin')
 // Создаёт шаблон метки
 var renderDraftPin = function (pin) {
   var userPin = template.cloneNode(true);
-  userPin.style.left = (pin.location.x - PIN_WIDTH) + 'px';
+  userPin.style.left = (pin.location.x - PIN_WIDTH / 2) + 'px';
   userPin.style.top = (pin.location.y - PIN_HEIGHT) + 'px';
   userPin.querySelector('img').src = pin.author.avatar;
   userPin.querySelector('img').alt = pin.offer.title;
@@ -94,5 +83,6 @@ var createPins = function (array) {
   }
   pins.appendChild(fragment);
 };
+
 // выводит объявления
 createPins(randomAd);
